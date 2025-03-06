72b56bea114adf2b8c6fcc72050a63c5251dc4d8
Gloria R <norimaqidoe@hotmail.com>
2025-03-05 20:53:08 -0600

    feat: add useForm hook for form

---
+++ src/hooks/useForm.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+function useForm (callback, defaults) {
+  const [input, setInput] = useState(defaults)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    callback(input)
+  }
+  const handleInputChange = (event) => {
+    const { name, value } = event.target
+    setInput({ ...input, [name]: value })
+  }
+  return {
+    input,
+    handleInputChange,
+    handleSubmit
+  }
+}
+
+export default useForm
