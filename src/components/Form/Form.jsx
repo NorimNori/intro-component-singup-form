544487540a922f62d0101edc98cce54f941e2df1
Gloria R <norimaqidoe@hotmail.com>
2025-03-05 21:14:31 -0600

    feat: make the form send the information

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,16 +1,62 @@
+import useForm from "../../hooks/useForm";
 import Button from "../Button/Button";
 import Input from "../Input/Input";
 import './Form.scss'
 
 const Form = () => {
+  const sendData = async (data) => {
+    console.log(data)
+  }
+
+  const { input, handleInputChange, handleSubmit } = useForm(sendData, {
+    first_name: '',
+    last_name: '',
+    email: '',
+    password: ''
+  })
   return (
     <section className="form-container" aria-labelledby="form-title">
       <h2 id="form-title" className="visually-hidden">Sign up for free trial</h2>
-      <form className="form" aria-describedby="form-terms">
-        <Input type="text" placeholder="First Name" />
-        <Input type="text" placeholder="Last Name" />
-        <Input type="email" placeholder="Email address" />
-        <Input type="password" placeholder="Password" />
+      <form 
+      onSubmit={handleSubmit}
+      className="form" 
+      aria-describedby="form-terms">
+        <Input 
+        type="text"
+        id="first_name"
+        name="first_name"
+        value={input.first_name}
+        onChange={handleInputChange}        
+        placeholder="First Name" 
+        />
+
+        <Input 
+        type="text"
+        id="last_name"
+        name="last_name"
+        value={input.last_name}
+        onChange={handleInputChange}  
+        placeholder="Last Name" 
+        />
+
+        <Input 
+        type="email"
+        id="email"
+        name="email"
+        value={input.email}
+        onChange={handleInputChange} 
+        placeholder="Email address" 
+        />
+
+        <Input 
+        type="password"
+        id="password"
+        name="password"
+        value={input.password}
+        onChange={handleInputChange} 
+        placeholder="Password" 
+        />
+
         <Button />
         <p id="form-terms" className="form__terms">
           By clicking the button, you are agreeing to our{" "}
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,11 +1,15 @@
 import './Input.scss'
 
-const Input = ({ type, placeholder }) => {
+const Input = ({ type, name, id, value, onChange, placeholder }) => {
   return (
     <label className="form__label">
       <span className="visually-hidden">{placeholder}</span>
       <input 
-      type={type} 
+      type={type}
+      id={id}
+      name={name}
+      value={value}
+      onChange={onChange}      
       placeholder={placeholder} 
       className="form__input form__input--error"
       aria-label={placeholder}
